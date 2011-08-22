12242d32f32803f405a7fac133b029c087083839
jumisz <juan.m.salamanca@gmail.com>
2011-08-23 00:40:37 +0100

    Fixed bad globals, improving line report, and adding block reporting

--- examples/sample.js
+++ examples/sample.js
@@ -1,13 +1,11 @@
 exports.sampleFunction = function () {
 console.log("Hello from sampleFunction"); 
-console.log(JSON.stringify(global.Stats)); 
 };
 
 exports.anotherFunction = function() {
 "This is a function".split(" ").forEach(function(word) {
 console.log("Printing: "+word); 
 });
-console.log(JSON.stringify(global.Stats)); 
 };
 
 
