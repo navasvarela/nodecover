4f57819be61816b382b06fcbf42a89b952d29db5
jumisz <juan.m.salamanca@gmail.com>
2011-08-25 17:11:00 +0100

    Fixed issue with source lines array, backing up stats in prep for report by module

--- examples/sample.js
+++ examples/sample.js
@@ -1,12 +1,15 @@
-exports.sampleFunction = function () {
+exports.sampleFunction = function () { 
 console.log("Hello from sampleFunction"); 
 };
 
-exports.anotherFunction = function() {
-"This is a function".split(" ").forEach(function(word) {
+exports.anotherFunction = function() { 
+"This is a function".split(" ").forEach(function(word) { 
 console.log("Printing: "+word); 
 });
 };
+
+
+
 
 
 
