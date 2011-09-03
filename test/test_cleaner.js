31a4e784d0f98ea129302f211948aa76790cbfc9
jumisz <juan.m.salamanca@gmail.com>
2011-09-03 09:02:21 +0100

    Improvements in cleaner.js

--- test/test_cleaner.js
+++ test/test_cleaner.js
@@ -20,39 +20,39 @@ module.exports = testCase({
 
 	"Should cleanup single statement for loops": function(test) {
 		var testLines = ['for (i=0;i<blah;i++)','console.log("Hello World");'];
-		var shouldBeLines = ['for (i=0;i<blah;i++) {','console.log("Hello World");}'];
+		var shouldBeLines = ['for (i=0;i<blah;i++){','console.log("Hello World");}'];
 		test.deepEqual(shouldBeLines,cleaner.cleanForLoops(testLines));
 		test.done();
 	},
 	"Should cleanup for loops with if statements": function(test) {
 		var testLines = ['for (i=0;i<blah;i++)','if (true) console.log("Hello World");'];
-		var shouldBeLines = ['for (i=0;i<blah;i++) {','if (true) {  console.log("Hello World"); } }'];
+		var shouldBeLines = ['for (i=0;i<blah;i++){','if (true){ console.log("Hello World");}}'];
 		test.deepEqual(shouldBeLines,cleaner.cleanForLoops(testLines));
 		test.done();
 	},
 	"Should cleanup single for loops with if-else statements": function(test) {
 		var testLines = ['for (i=0;i<blah;i++)','if (true) console.log("Hello World");', 'else blah;'];
-		var shouldBeLines = ['for (i=0;i<blah;i++) {','if (true) {  console.log("Hello World"); } ', 'else { blah; } }'];
+		var shouldBeLines = ['for (i=0;i<blah;i++){','if (true){ console.log("Hello World");}', 'else{ blah;}}'];
 		test.deepEqual(shouldBeLines,cleaner.clean(testLines));
 		test.done();
 	},
 	"Should cleanup nested for loops": function(test) {
 		
 		var testLines = ['for (i=0;i<blah;i++)','for(j=0;j<blablah;j++) ', 'do blah;'];
-		var shouldBeLines = ['for (i=0;i<blah;i++) {','for(j=0;j<blablah;j++)  {', 'do blah;}}'];
+		var shouldBeLines = ['for (i=0;i<blah;i++){','for(j=0;j<blablah;j++) {', 'do blah;}}'];
 		test.deepEqual(shouldBeLines,cleaner.cleanForLoops(testLines));
 		test.done();
 	},
 	"Should cleanup nested for loops with if-else": function(test) {
 		
 		var testLines = ['for (i=0;i<blah;i++)','for(j=0;j<blablah;j++) ', 'if(echo) blah;', 'else bloo;'];
-		var shouldBeLines = ['for (i=0;i<blah;i++) {','for(j=0;j<blablah;j++)  {', 'if(echo) {  blah; } ', 'else { bloo; } }}'];
+		var shouldBeLines = ['for (i=0;i<blah;i++){','for(j=0;j<blablah;j++) {', 'if(echo){ blah;}', 'else{ bloo;}}}'];
 		test.deepEqual(shouldBeLines,cleaner.clean(testLines));
 		test.done();
 	},
 	"Should cleanup nested for loops with if-else blocks": function(test) {
 		var testLines = ['for (i=0;i<blah;i++)','for(j=0;j<blablah;j++) ', 'if(echo) {','blah;', '} else bloo;'];
-		var shouldBeLines = ['for (i=0;i<blah;i++) {','for(j=0;j<blablah;j++)  {', 'if(echo) {','blah;', '} else bloo;}}'];
+		var shouldBeLines = ['for (i=0;i<blah;i++){','for(j=0;j<blablah;j++) {', 'if(echo) {','blah;', '} else{ bloo;}}}'];
 		test.deepEqual(shouldBeLines,cleaner.clean(testLines));
 		test.done();
 	},
@@ -77,14 +77,21 @@ module.exports = testCase({
 	},
 	"Should clean if-else block with nested for loop": function(test) {
 		var testLines = ['if(echo) ','for (too)',' blah;', 'else', 'bloo;'];
-		var shouldBeLines = ['if(echo) { ','for (too) {',' blah;} } ', 'else { ', 'bloo; } '];
+		var shouldBeLines = ['if(echo){','for (too){',' blah;}}', 'else{', 'bloo;}'];
+		test.deepEqual(shouldBeLines,cleaner.clean(testLines));
+		test.done();
+		
+	},
+	"Should clean else block with nested for loop": function(test) {
+		var testLines = ['if(echo) ',' blah;', 'else', 'for(too)',' bloo;'];
+		var shouldBeLines = ['if(echo){',' blah;}', 'else{','for(too){', ' bloo;}}'];
 		test.deepEqual(shouldBeLines,cleaner.clean(testLines));
 		test.done();
 		
 	},
 	"Should cleanup for loop with if-else statements in several lines": function(test) {
 		var testLines = ['for (i=0;i<blah;i++)','if(echo)',' blah;', 'else',' bloo;'];
-		var shouldBeLines = ['for (i=0;i<blah;i++) {','if(echo) { ',' blah; } ', 'else { ',' bloo; } }'];
+		var shouldBeLines = ['for (i=0;i<blah;i++){','if(echo){',' blah;}', 'else{',' bloo;}}'];
 		test.deepEqual(shouldBeLines,cleaner.clean(testLines));
 		test.done();
 
@@ -103,9 +110,28 @@ module.exports = testCase({
 	"Should clean if-else blocks inside for block": function(test) {
 		
 		var testLines = ['for (i=0;i<blah;i++) {','if(echo)',' blah;', 'else',' bloo; }'];
-		var shouldBeLines = ['for (i=0;i<blah;i++) {','if(echo) { ',' blah; } ', 'else { ',' bloo; } } '];
+		var shouldBeLines = ['for (i=0;i<blah;i++) {','if(echo){',' blah;}', 'else{',' bloo; }}'];
 		test.deepEqual(shouldBeLines,cleaner.clean(testLines));
 		test.done();
-	}
+	},
+	"Should not find if-else blocks inside comments": function(test) {
+		var testLines = ['//if the world was flat','if(echo)',' blah;// another if ', 'else',' bloo; '];
+		var shouldBeLines = ['//if the world was flat','if(echo){',' blah;}', 'else{',' bloo; }'];
+		test.deepEqual(shouldBeLines,cleaner.clean(testLines));
+		test.done();
+
+	},
+	"Should not clean plain for loops":  function(test) {
+		var testLines = ['function bitSize(x) {','var j,z,w;','for (j=x.length-1; (x[j]==0) && (j>0); j--);','for (z=0,w=x[j]; w; (w>>=1),z++);','z+=bpe*j;'];
+		test.strictEqual(testLines,cleaner.clean(testLines));
+		test.done();
+	},
+	"Should clean if-for-if blocks":  function(test) {
+		var testLines = ['if (x.length>y.length) {','for (;i<x.length;i++)','if (x[i])','return 0;','} else {','for (;i<y.length;i++)','if (y[i])','return 0;','}'];
+		var shouldBeLines = ['if (x.length>y.length) {','for (;i<x.length;i++){','if (x[i]){','return 0;}}','} else {','for (;i<y.length;i++){','if (y[i]){','return 0;}}','}'];
+		test.deepEqual(shouldBeLines,cleaner.clean(testLines));	
+		test.done();
+	},
+ 
 	
 });
